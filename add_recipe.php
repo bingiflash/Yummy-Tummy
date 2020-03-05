2bc6e66db804f6737153bbf959acedd9b9b8f116
bingiflash <37812220+bingiflash@users.noreply.github.com>
2020-03-04 19:22:51 -0500

    back end sync with ajax

--- add_recipe.php
+++ add_recipe.php
@@ -5,19 +5,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <script  src="https://code.jquery.com/jquery-3.4.1.min.js"  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   <title>Add Recipie</title>
 </head>
 
-<body >
+<body>
   <div class="container-fluid">
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
-<h1 class="display-4">Add Recipe</h1>
-<p class="lead">Enter ingredients required and their quantity</p>
-</div>
+        <h1 class="display-4">Add Recipe</h1>
+        <p class="lead">Enter ingredients required and their quantity</p>
+      </div>
     </div>
   </div>
   <div class="container">
@@ -25,8 +25,8 @@
       <div class="input-group-prepend">
         <span class="input-group-text">Recipe</span>
       </div>
-      <input  type="text" class="form-control" id="recipe_name" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-      <div class="input-group-append btn-group btn-group-toggle" >
+      <input type="text" class="form-control" id="recipe_name" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+      <div class="input-group-append btn-group btn-group-toggle">
         <label class="btn btn-outline-success tbtn " id='veg_btn'>
           <input type="radio" name="type" id="veg" checked> Veg
         </label>
@@ -38,7 +38,7 @@
     <hr>
     <div class="input-group mt-2">
       <div class="input-group-prepend">
-        <span class="input-group-text" >url</span>
+        <span class="input-group-text">url</span>
       </div>
       <input type="text" class="form-control" id="url" placeholder="Enter URL for reference" aria-label="Username" aria-describedby="basic-addon1">
     </div>
@@ -49,35 +49,35 @@
       <textarea class="form-control" id="note" aria-label="With textarea"></textarea>
     </div>
     <div class="container">
-    <?php
-          include("backup\dbconfig.php"); //db configuration
-          // Check connection
-          if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-          }
+      <?php
+      include("backup\dbconfig.php"); //db configuration
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
 
-          $sql = "SELECT * FROM ingredient ORDER BY ingredient.item ASC";
-          $result = $conn->query($sql);
+      $sql = "SELECT * FROM ingredient ORDER BY ingredient.item ASC";
+      $result = $conn->query($sql);
 
-          if ($result->num_rows > 0) {
-            echo "<datalist id='ingredients_list'>";
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-              $item = $row["item"];
-              $item = str_replace(' ', '', $item);
-              echo '<option id = "'.$item.'" label="' . $row['id'] . '" value="' . $row["item"] . '">';
-            }
-            echo "</datalist>";
-          } else {
-            echo "0 results";
-          }
-          $conn->close();
-          ?>
-      <div class="container-fluid ing" >
+      if ($result->num_rows > 0) {
+        echo "<datalist id='ingredients_list'>";
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+          $item = $row["item"];
+          $item = str_replace(' ', '', $item);
+          echo '<option id = "' . $item . '" label="' . $row['id'] . '" value="' . $row["item"] . '">';
+        }
+        echo "</datalist>";
+      } else {
+        echo "0 results";
+      }
+      $conn->close();
+      ?>
+      <div class="container-fluid ing">
         <!-- Hidden ingredient thread  -->
         <div class="row input-group mb-1" id="ing_0" style="display:none;">
-          <input class=" col-lg-7 col-md-7 col-sm-12 custom-select mr-1"  aria-label="" type="text" list="ingredients_list" placeholder="Ingredient"> 
-          <input class=" col-lg-2 col-md-2 col-sm-3 form-control mr-1" type="number" min="1"  aria-label="" placeholder="Quantity">
+          <input class=" col-lg-7 col-md-7 col-sm-12 custom-select mr-1" aria-label="" type="text" list="ingredients_list" placeholder="Ingredient">
+          <input class=" col-lg-2 col-md-2 col-sm-3 form-control mr-1" type="number" min="1" aria-label="" placeholder="Quantity">
           <select class=" col-lg-2 col-md-2 col-sm-3 custom-select" aria-label="">
             <option hidden>Units</option>
             <option value="pieces">Pieces</option>
@@ -90,7 +90,7 @@
             <button class="btn btn-outline-danger remove_ing " type="button">-</button>
           </div>
         </div>
-        <!-- end Hidden ingredient thread-->        
+        <!-- end Hidden ingredient thread-->
       </div>
       <div class="row mt-1">
         <div class="col-12 ">
@@ -99,89 +99,90 @@
       </div>
     </div>
   </div>
-<script>
+  <script>
+    var ing_id = 0;
+    $('.remove_ing').click(remove_ingredient);
+    $('#add').click(send_data);
+    add_ingredient();
+    non_veg();
+    $('.tbtn').click(non_veg);
 
-var ing_id  = 0;
-$('.remove_ing').click(remove_ingredient);
-$('#add').click(send_data);
-add_ingredient();
-non_veg();
-$('.tbtn').click(non_veg);
-function non_veg(){
-  if($('#nonveg').is(':checked')){
-    //console.log($('#nonveg').is(':checked'));
-    $("#nonveg_btn").removeClass("btn-outline-warning");
-    $("#veg_btn").removeClass("btn-success");
-    $("#nonveg_btn").addClass("btn-warning");
-    $("#veg_btn").addClass("btn-outline-success");
-  }
-  else{
-    $("#nonveg_btn").removeClass("btn-warning");
-    $("#veg_btn").removeClass("btn-outline-success");
-    $("#nonveg_btn").addClass("btn-outline-warning");
-    $("#veg_btn").addClass("btn-success");
-  }
-  
+    function non_veg() {
+      if ($('#nonveg').is(':checked')) {
+        //console.log($('#nonveg').is(':checked'));
+        $("#nonveg_btn").removeClass("btn-outline-warning");
+        $("#veg_btn").removeClass("btn-success");
+        $("#nonveg_btn").addClass("btn-warning");
+        $("#veg_btn").addClass("btn-outline-success");
+      } else {
+        $("#nonveg_btn").removeClass("btn-warning");
+        $("#veg_btn").removeClass("btn-outline-success");
+        $("#nonveg_btn").addClass("btn-outline-warning");
+        $("#veg_btn").addClass("btn-success");
+      }
 
-}
-function remove_ingredient(){
-  var ing_remove_id = '#'+$(this).parent().parent().attr("id");
-  //console.log(ing_remove_id);
-  $(ing_remove_id).remove();
-  var list_ing = $('.ing').children();
-  //console.log(list_ing.length);
-  if(list_ing.length <= 1){
-    add_ingredient(); 
-  }
-}
 
-function add_ingredient(){
-  var ing_0 =  $('#ing_0').html();
-  ing_id+=1;
-  var html =  `<div class="row input-group ing_details mb-1" id="ing_${ing_id}">${ing_0}</div>`;
-  $(".ing").append(html);
-  $('.remove_ing').click(remove_ingredient);
-}
-function send_data(){
-  var ing_obj = {};
-  var ing_ar = [];
-  var list_ing = $('.ing').children();
-  for (i = 1; i < list_ing.length; i++){
-    ing_key = list_ing[i].children[0].value;
-    ing_key = "#"+ing_key.replace( /\s/g, '');
-    ing_obj["ing_id"]=$(ing_key).attr("label");
-    ing_obj["quantity"]=list_ing[i].children[1].value;
-    ing_obj["units"]=list_ing[i].children[2].value;
-    ing_ar.push(ing_obj);
-    ing_obj = {};
-  }
-  if($('#nonveg').is(':checked')){
-    typ = "Nonveg";
-  }else{
-    typ = "Veg";
-  }
-  var recipe_data = {
-    recipe_name:$("#recipe_name").val(),
-    type:typ,
-    url:$("#url").val(),
-    note:$("#note").val(),
-    ing_list:ing_ar
-  }
-  $.ajax({
-    type: "POST",
-    url: "add_recipe_backend.php",
-    data: {
-      data: JSON.stringify(recipe_data)
-    },
-    success: function (ee) {
-      alert(ee);
-    },
-    error:()=>{
-      alert('AJAX Error!');
     }
-  });
-}
-</script>
+
+    function remove_ingredient() {
+      var ing_remove_id = '#' + $(this).parent().parent().attr("id");
+      //console.log(ing_remove_id);
+      $(ing_remove_id).remove();
+      var list_ing = $('.ing').children();
+      //console.log(list_ing.length);
+      if (list_ing.length <= 1) {
+        add_ingredient();
+      }
+    }
+
+    function add_ingredient() {
+      var ing_0 = $('#ing_0').html();
+      ing_id += 1;
+      var html = `<div class="row input-group ing_details mb-1" id="ing_${ing_id}">${ing_0}</div>`;
+      $(".ing").append(html);
+      $('.remove_ing').click(remove_ingredient);
+    }
+
+    function send_data() {
+      var ing_obj = {};
+      var ing_ar = [];
+      var list_ing = $('.ing').children();
+      for (i = 1; i < list_ing.length; i++) {
+        ing_key = list_ing[i].children[0].value;
+        ing_key = "#" + ing_key.replace(/\s/g, '');
+        ing_obj["ing_id"] = $(ing_key).attr("label");
+        ing_obj["quantity"] = list_ing[i].children[1].value;
+        ing_obj["units"] = list_ing[i].children[2].value;
+        ing_ar.push(ing_obj);
+        ing_obj = {};
+      }
+      if ($('#nonveg').is(':checked')) {
+        typ = "Nonveg";
+      } else {
+        typ = "Veg";
+      }
+      var recipe_data = {
+        recipe_name: $("#recipe_name").val(),
+        type: typ,
+        url: $("#url").val(),
+        note: $("#note").val(),
+        ing_list: ing_ar
+      }
+      $.ajax({
+        type: "POST",
+        url: "add_recipe_backend.php",
+        data: {
+          data: JSON.stringify(recipe_data)
+        },
+        success: function(ee) {
+          alert(ee.msg);
+        },
+        error: () => {
+          alert('AJAX Error!');
+        }
+      });
+    }
+  </script>
 </body>
 
 </html>
--- add_recipe_backend.php
+++ add_recipe_backend.php
@@ -4,41 +4,51 @@ if (!$conn)
     die("Connection failed: " . mysqli_connect_error());
 
 // JSON init or from POST
-$QueryData = array();
-$QueryData['receipe_name'] = "Tomato Curry";
-$QueryData['type'] = 'veg';
-$QueryData['url'] = "https://www.youtube.com/watch?v=6d064RiWVJM";
-// $QueryData['notes'] = '';
-$QueryData['ing_list'] = array(
-    array("ing_id" => "1", "quantity" => "3", "units" => "Pieces"),
-    array("ing_id" => "9", "quantity" => "1", "units" => "Pieces"),
-    array("ing_id" => "63", "quantity" => "1", "units" => "T-spoons"),
-    array("ing_id" => "65", "quantity" => "1", "units" => "T-spoons"),
-    array("ing_id" => "20", "quantity" => "10", "units" => "Pieces")
-);
+// $QueryData = array();
+// $QueryData['recipe_name'] = "Tomato Curry";
+// $QueryData['type'] = 'veg';
+// $QueryData['url'] = "https://www.youtube.com/watch?v=6d064RiWVJM";
+// // $QueryData['notes'] = '';
+// $QueryData['ing_list'] = array(
+//     array("ing_id" => "1", "quantity" => "3", "units" => "Pieces"),
+//     array("ing_id" => "9", "quantity" => "1", "units" => "Pieces"),
+//     array("ing_id" => "63", "quantity" => "1", "units" => "T-spoons"),
+//     array("ing_id" => "65", "quantity" => "1", "units" => "T-spoons"),
+//     array("ing_id" => "20", "quantity" => "10", "units" => "Pieces")
+// );
 //JSON done
 
-$recipe_query = "INSERT INTO recipe VALUES (NULL, \"" . $QueryData['receipe_name'] . "\", \"" . $QueryData['type'] . "\", \"" . $QueryData['url'] . "\")";
+$QueryData = json_decode($_POST["data"], true);
+$res_suc = false;
+
+$recipe_query = "INSERT INTO recipe VALUES (NULL, \"" . $QueryData['recipe_name'] . "\", \"" . $QueryData['type'] . "\", \"" . $QueryData['url'] . "\", \"" . $QueryData['url'] . "\")";
 $result = mysqli_query($conn, $recipe_query);
 
 if ($result) {
-    echo "Added a Recipe <br>";
+    $res_suc = true;
 } else {
-    echo "Failed to add a Recipe <br>";
+    $res_suc = false;
 }
 
-$id_query = "SELECT id  FROM recipe WHERE name LIKE \"" . $QueryData['receipe_name'] . "\"";
+$id_query = "SELECT id  FROM recipe WHERE name LIKE \"" . $QueryData['recipe_name'] . "\"";
 $result = mysqli_query($conn, $id_query);
 $recipe_id =  mysqli_fetch_assoc($result)["id"];
-
 foreach ($QueryData['ing_list'] as &$step) {
     $recipe_ing_query = "INSERT INTO recipe_ingredient VALUES (NULL, \"" . $recipe_id . "\", \"" . $step['ing_id'] . "\", \"" . $step['quantity'] . "\", \"" . $step['units'] . "\")";
     $result = mysqli_query($conn, $recipe_ing_query);
     if ($result) {
-        echo "Added a Recipe Step <br>";
+        $res_suc = true;
     } else {
-        echo "Unable to a add a Recipe Step <br>";
+        $res_suc = false;
+        break;
     }
 }
-
+$rep_msg = array();
+if ($res_suc) {
+    $rep_msg['msg'] = "Recipe Added Successfully";
+} else {
+    $rep_msg['msg'] = "Failed to add recipe";
+}
+header('Content-type: application/json');
+echo json_encode($rep_msg);
 mysqli_close($conn);
